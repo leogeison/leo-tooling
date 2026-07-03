0a00cea652be2b4a9b587cb8a73be0278a7ceb03
Léo Geison <leogeisom@gmail.com>
2026-07-03 16:40:21 -0300

    feat: add react eslint plugins

--- packages/eslint-config/src/react.ts
+++ packages/eslint-config/src/react.ts
@@ -1,10 +1,40 @@
+import reactPlugin from 'eslint-plugin-react'
+import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
+
 import base from './base.js'
 
-export default [
+const reactConfig = [
   ...base,
   {
+    files: ['**/*.{jsx,tsx}'],
+    plugins: {
+      react: reactPlugin,
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+    },
+    languageOptions: {
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+    },
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
     rules: {
-      'react/react-in-jsx-scope': 'off'
-    }
-  }
-]
+      ...reactPlugin.configs.recommended.rules,
+      ...reactHooks.configs.recommended.rules,
+      'react/react-in-jsx-scope': 'off',
+      'react-refresh/only-export-components': [
+        'warn',
+        { allowConstantExport: true },
+      ],
+    },
+  },
+] as unknown as typeof base
+
+export default reactConfig
