289271b20c70ec3798918a56b6d12dbe2283c719
Léo Geison <leogeisom@gmail.com>
2026-07-04 07:51:18 -0300

    build: configure tsup

---
+++ packages/eslint-config/tsup.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from 'tsup'
+
+export default defineConfig({
+  entry: ['src/base.ts', 'src/node.ts', 'src/react.ts', 'src/index.ts'],
+  format: ['esm'],
+  dts: true,
+  sourcemap: true,
+  clean: true,
+  target: 'es2022',
+  splitting: false,
+  bundle: false,
+})
